e786d632102822b877ade5198f8f467f98e9f656
Michael Donohoe <donohoe@gmail.com>
2017-12-23 17:02:55 -0500

    Updates for main page and also restored examples

--- public/embeds.php
+++ public/embeds.php
@@ -21,23 +21,18 @@ $response = $document->Run( "2PACX-1vSxgb03MiCWgXywb90TyBNuU8jk-jofX0zoshuScDNyP
 <head>
 	<meta charset="utf-8" />
 	<title><?php print $response["page"]["title"]; ?></title>
-	<link rel="stylesheet" type="text/css" href="style.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="css/styles.css" media="all" />
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0" />
 	<style>
-	html, body {
-		font-family: "Arial";
-	}
-	img {
-		width: 100%;
-		max-width: 400px;
-	}
+
 	<?php print $response["styles"]; ?>
 	</style>
 </head>
 <body>
-	<div id="navigation">
+	<header>
+		<div id="navigation">
 		<?php
 			foreach ($response["page"]["index"] as $k => $title) {
 				if (!empty($title)) {
@@ -45,7 +40,8 @@ $response = $document->Run( "2PACX-1vSxgb03MiCWgXywb90TyBNuU8jk-jofX0zoshuScDNyP
 				}
 			}
 		?>
-	</div>
+		</div>
+	</header>
 	<div id="content">
 		<?php print $response["content"]; ?>
 	</div>
